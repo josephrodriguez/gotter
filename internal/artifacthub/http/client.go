0b8b75d1f91096a05a2ecd6d111bcee6b2c8190b
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-12-03 01:21:01 +0200

    Create ArtifactHub webhook models

---
+++ internal/artifacthub/http/client.go
@@ -0,0 +1,60 @@
+package http
+
+import (
+	"net/http"
+	"time"
+)
+
+type WebhookResponse struct {
+	Id       string           `json:"webhook_id"`
+	Name     string           `json:"id"`
+	URL      string           `json:"url"`
+	Active   bool             `json:"active"`
+	Packages []WebhookPackage `json:"packages"`
+}
+
+type WebhookPackage struct {
+	Id             string            `json:"package_id"`
+	Name           string            `json:"name"`
+	NormalizedName string            `json:"normalized_name"`
+	Description    string            `json:"description"`
+	Version        string            `json:"version"`
+	AppVersion     string            `json:"app_version"`
+	LogoImageId    string            `json:"logo_image_id"`
+	License        string            `json:"license"`
+	Deprecated     bool              `json:"deprecated"`
+	Signed         bool              `json:"signed"`
+	Stars          int               `json:"stars"`
+	Repository     WebhookRepository `json:"repository"`
+}
+
+type WebhookRepository struct {
+	Id                      string `json:"repository_id"`
+	Name                    string `json:"name"`
+	URL                     string `json:"url"`
+	Private                 bool   `json:"private"`
+	Kind                    int    `json:"kind"`
+	VerifiedPublisher       bool   `json:"verified_publisher"`
+	Official                bool   `json:"official"`
+	ScannerDisabled         bool   `json:"scanner_disabled"`
+	OrganizationName        string `json:"organization_name"`
+	OrganizationDisplayName string `json:"organization_display_name"`
+}
+
+type Client struct {
+	BaseURL    string
+	HTTPClient *http.Client
+	ApiKey     string
+	ApiSecret  string
+}
+
+func NewClient(baseUrl, apiKey, apiSecret string) Client {
+	return Client{
+		BaseURL:   baseUrl,
+		ApiKey:    apiKey,
+		ApiSecret: apiSecret,
+		HTTPClient: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
+}
